5cc3c0006c2808e8b3d7daee4ed2accd372bb6a1
Nevette <agnieszka.dorota.dzieciol@gmail.com>
2018-10-14 18:42:01 +0200

    Added connection between menu and plan route activity, fixed layout in plan route activity

--- app/src/main/java/ad/optiroad/MenuActivity.java
+++ app/src/main/java/ad/optiroad/MenuActivity.java
@@ -1,13 +1,31 @@
 package ad.optiroad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button buttonPlanRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        buttonPlanRoute = (Button) findViewById(R.id.buttonPlanRoute);
+        buttonPlanRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlanRouteActivity();
+            }
+        });
+    }
+
+    public void openPlanRouteActivity(){
+        Intent i = new Intent(this, PlanRouteActivity.class);
+        startActivity(i);
     }
 }
