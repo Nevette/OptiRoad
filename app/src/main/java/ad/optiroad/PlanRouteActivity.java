a43a65807970ce405ac597326d9a2efff9e8ba7f
Nevette <agnieszka.dorota.dzieciol@gmail.com>
2018-10-25 20:41:51 +0200

    Added navigation between PlanRouteActivity and MapsActivity

--- app/src/main/java/ad/optiroad/PlanRouteActivity.java
+++ app/src/main/java/ad/optiroad/PlanRouteActivity.java
@@ -1,13 +1,30 @@
 package ad.optiroad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlanRouteActivity extends AppCompatActivity {
+
+    private Button buttonNavigate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_route);
+
+        buttonNavigate = (Button) findViewById(R.id.buttonNavigate);
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNavigateActivity();
+            }
+        });
+    }
+    public void openNavigateActivity() {
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 }
