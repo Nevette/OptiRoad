65e30e7281ea4ce6180af2f66b4f59f21eb22068
Nevette <agnieszka.dorota.dzieciol@gmail.com>
2018-10-28 22:19:52 +0100

    Added translation string address to position on map

--- app/src/main/java/ad/optiroad/MapsActivity.java
+++ app/src/main/java/ad/optiroad/MapsActivity.java
@@ -1,7 +1,10 @@
 package ad.optiroad;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,37 +13,48 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String startingPoint;
+    private static final String TAG = "MapsActivity";
+    Geocoder coder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        coder = new Geocoder(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // Get starting point passed in PlanRouteActivity
+        startingPoint = (String) getIntent().getSerializableExtra("startingPoint");
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        try {
+            List<Address> positionList = coder.getFromLocationName(startingPoint, 1);
+            if (!positionList.isEmpty()) {
+                LatLng address = addressToLatLng(positionList.get(0));
+                mMap.addMarker(new MarkerOptions().position(address).title("Marker in " + startingPoint));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(address));
+            }
+
+        }
+        catch (Exception e){
+            Toast toast=Toast.makeText(getApplicationContext(),"Cannot find given address",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+    private LatLng addressToLatLng(Address address){
+        return new LatLng(address.getLatitude(), address.getLongitude());
     }
 }
--- app/src/main/java/ad/optiroad/PlanRouteActivity.java
+++ app/src/main/java/ad/optiroad/PlanRouteActivity.java
@@ -5,15 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PlanRouteActivity extends AppCompatActivity {
 
     private Button buttonNavigate;
+    private EditText inputStartingPoint;
+    private static final String TAG = "PlanRouteActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_route);
+
+        inputStartingPoint = (EditText) findViewById(R.id.inputStartingPoint);
+        inputStartingPoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Delete content of input field and set it to empty
+                String startingPoint = inputStartingPoint.getText().toString();
+                if (startingPoint.equals("Type your starting point")) {
+                    inputStartingPoint.setText("");
+                }
+            }});
 
         buttonNavigate = (Button) findViewById(R.id.buttonNavigate);
         buttonNavigate.setOnClickListener(new View.OnClickListener() {
@@ -23,8 +38,11 @@ public class PlanRouteActivity extends AppCompatActivity {
             }
         });
     }
+
     public void openNavigateActivity() {
         Intent i = new Intent(this, MapsActivity.class);
+        // Pass starting point to MapsActivity
+        i.putExtra("startingPoint", inputStartingPoint.getText().toString());
         startActivity(i);
     }
 }
